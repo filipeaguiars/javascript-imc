c682556767505866d2c26bb9d9fb579bb0c7ce93
filipe <filipe.aguiar_s@hotmail.com>
2021-04-07 00:17:37 -0300

    added some javascript logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,21 @@ function calcIMC() {
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-        let imc = peso / (altura * altura);
+        let imc = peso.value / (altura.value * altura.value);
 
-
-        console.log(imc);
-
-
-        resultado.innerHTML += `<p> O IMC é ${imc}</p>`
-        console.log(IMC);
-
+        if (imc < 18.5) {
+            resultado.innerHTML += `<p> Abaixo do Peso ${imc.toFixed(2)}</p>`
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultado.innerHTML = `<p> Peso Normal ${imc.toFixed(2)}</p>`
+        } else if (imc >= 25 && imc <= 29.9) {
+            resultado.innerHTML = `<p class="sobrepeso"> Sobrepeso ${imc.toFixed(2)}</p>`
+        } else if (imc >= 30 && imc <= 34.9) {
+            resultado.innerHTML = `<p class="obesidade"> Obesidade Grau 1 ${imc.toFixed(2)}</p>`
+        } else if (imc >= 35 && imc <= 39.9) {
+            resultado.innerHTML = `<p class="obesidade">Obesidade Grau 2</p>`
+        } else {
+            resultado.innerHTML = `<p class="obesidade">Obesidade Grau 3</p>`
+        }
     }
 
     form.addEventListener('submit', recebeEventoForm)
